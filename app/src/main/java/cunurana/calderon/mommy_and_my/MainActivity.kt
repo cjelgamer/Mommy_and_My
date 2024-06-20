668c0700a7f44893dd4533199576c5d54af3813a
cjelgamer <cjelgamercc@gmail.com>
2024-06-20 08:52:45 -0500

    crear_cuenta_modificaciones commit

--- app/src/main/java/cunurana/calderon/mommy_and_my/MainActivity.kt
+++ app/src/main/java/cunurana/calderon/mommy_and_my/MainActivity.kt
@@ -1,21 +1,20 @@
 package cunurana.calderon.mommy_and_my
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
+        val button = findViewById<Button>(R.id.btn_sign_up)
+
+        button.setOnClickListener {
+            val intent = Intent(this, Crear_cuenta::class.java)
+            startActivity(intent)
         }
     }
 }
