96cfbe6501d9bdb512744f8806de562113bc4caf
cjelgamer <cjelgamercc@gmail.com>
2024-06-23 20:30:04 -0500

    cambios_para_boton_sign_up commit

--- app/src/main/java/cunurana/calderon/mommy_and_my/Crear_cuenta.kt
+++ app/src/main/java/cunurana/calderon/mommy_and_my/Crear_cuenta.kt
@@ -1,17 +1,19 @@
 package cunurana.calderon.mommy_and_my
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Crear_cuenta : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_cuenta)
 
-        // Verifica que el id main exista en el layout activity_crear_cuenta
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        // Verifica que el id rectangle_register exista en el layout activity_crear_cuenta
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rectangle_register)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/cunurana/calderon/mommy_and_my/MainActivity.kt
+++ app/src/main/java/cunurana/calderon/mommy_and_my/MainActivity.kt
@@ -1,21 +1,27 @@
 package cunurana.calderon.mommy_and_my
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+//import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.btn_sign_up)
-
-        button.setOnClickListener {
+        val btn_sign_up = findViewById<Button>(R.id.btn_sign_up)
+        //Toast.makeText(applicationContext,"Me presionaste......",Toast.LENGTH_SHORT).show()
+        btn_sign_up.setOnClickListener {
             val intent = Intent(this, Crear_cuenta::class.java)
             startActivity(intent)
         }
     }
+
 }
+
 
