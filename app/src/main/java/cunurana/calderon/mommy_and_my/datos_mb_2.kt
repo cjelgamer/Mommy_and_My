7b7623059e6d5a271f7ffb7cb15dac817c356846
cjelgamer <cjelgamercc@gmail.com>
2024-06-29 23:07:00 -0500

    datos_mb_full commit

---
+++ app/src/main/java/cunurana/calderon/mommy_and_my/datos_mb_2.kt
@@ -0,0 +1,20 @@
+package cunurana.calderon.mommy_and_my
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class datos_mb_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_datos_mb2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
