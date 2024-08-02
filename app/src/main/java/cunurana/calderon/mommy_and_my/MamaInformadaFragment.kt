3992cba981c74ef93696d00ffc27bcd848fd9422
Daniela Lucía <danlulatorre82@gmail.com>
2024-08-02 01:02:27 -0500

    modulo informativo

--- app/src/main/java/cunurana/calderon/mommy_and_my/MamaInformadaFragment.kt
+++ app/src/main/java/cunurana/calderon/mommy_and_my/MamaInformadaFragment.kt
@@ -9,12 +9,11 @@ import android.view.ViewGroup
 
 class MamaInformadaFragment : Fragment() {
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mama_informada, container, false)
     }
-
 }
